b2c6b8e07cc2fe9ba85b0c8ed4f85b8c74d94965
kempluo <kempluo@tencent.com>
2025-05-13 15:08:07 +0800

    Initial Commit

---
+++ iOSLearn/iOSLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSLearnApp.swift
+//  iOSLearn
+//
+//  Created by kempluo on 2025/5/13.
+//
+
+import SwiftUI
+
+@main
+struct iOSLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
