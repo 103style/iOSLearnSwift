e935eb57943ca31324d5b5eaf3f2510d2cca8a88
103style <wtimexiaoke@gmail.com>
2025-05-15 14:39:13 +0800

    Enum

--- playground/Enum.playground/Contents.swift
+++ playground/Enum.playground/Contents.swift
@@ -8,7 +8,7 @@ enum Number {
 //    case two
 //    case three
 
-    case one, two, three
+    case one, two, three // 默认从0开始
 }
 
 var myNumber = Number.one
@@ -22,4 +22,54 @@ case .two:
     print("two")
 case .three:
     print("three")
+}
+
+// 自定义枚举的值
+enum Type: String {
+    case one = "TypeOne"
+    case two = "TypeTwo"
+    case three = "TypeThree"
+
+    func getMsg() -> String {
+        switch self { // self代表当前值
+        case .one:
+            return "ONE"
+        case .two:
+            return "TWO"
+        case .three:
+            return "THREE"
+        }
+    }
+}
+
+var myType = Type.one
+print("rawValue = \(myType.rawValue)") // TypeOne
+
+var testType = Type(rawValue: "TypeOne")
+if testType == Type.one {
+    print("correct value")
+}
+
+print(myType.getMsg())
+
+// ---------
+
+enum MyCharacters {
+    case number(Int, String)
+    case letter(Character, String)
+}
+
+var character = MyCharacters.number(1, "Number One")
+
+switch character {
+case .number(let value, let des):
+    print("value:\(value), des:\(des)") // value:1, des:Number One
+case .letter(let value, let des):
+    print("value:\(value), des:\(des)")
+}
+
+// if中读取关联值
+if case .number(let number, let des) = character {
+    print("number:\(number)")
+    print("des:\(des)")
 }
