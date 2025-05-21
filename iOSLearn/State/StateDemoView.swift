4732f6b045db671740a4475ea70d312f296401c9
103style <wtimexiaoke@gmail.com>
2025-05-21 14:31:24 +0800

    @Binding修饰变量来实现双向绑定

--- iOSLearn/State/StateDemoView.swift
+++ iOSLearn/State/StateDemoView.swift
@@ -3,6 +3,7 @@
 //  iOSLearn
 //
 //  @State声明状态变量，视图使用变量，修改状态变量，视图则会自动刷新
+//  @Binding，当状态属性在不同view中时， 其他View的的双向绑定属性用 @Binding 声明
 //
 //  Created by kempluo on 2025/5/21.
 //
@@ -19,6 +20,7 @@ struct StateDemoView: View {
 
     @State private var titleInput: String = ""
 
+//    // 单向绑定
 //    var body: some View {
 //        Text(title).padding()
 //
@@ -32,21 +34,52 @@ struct StateDemoView: View {
 //        Spacer()
 //    }
 
+    // 双向绑定
+//    var body: some View {
+//        Text(title).padding()
+//
+//        TextField("Insert Title", text: $titleInput) // $titleInput：双向绑定
+//            .textFieldStyle(.roundedBorder)
+//            .padding(.horizontal, 4)
+//
+//        Button(action: {
+//            title = titleInput
+//            titleInput = ""
+//        }, label: {
+//            Text("change title")
+//        }).padding()
+//
+//        Spacer()
+//    }
+
+    // 当状态属性在不同view中时， 其他View的的双向绑定属性用 @Binding 声明
     var body: some View {
-        Text(title).padding()
+        VStack {
+            HeadView(title: title, titleInput: $titleInput)
+            Button(action: {
+                title = titleInput
+                titleInput = ""
+            }, label: {
+                Text("change title")
+            }).padding()
 
-        TextField("Insert Title", text: $titleInput) // $titleInput：双向绑定
-            .textFieldStyle(.roundedBorder)
-            .padding(.horizontal, 4)
+            Spacer()
+        }.padding()
+    }
+}
 
-        Button(action: {
-            title = titleInput
-            titleInput = ""
-        }, label: {
-            Text("change title")
-        }).padding()
+// @binding
+struct HeadView: View {
+    var title: String
+    @Binding var titleInput: String
 
-        Spacer()
+    var body: some View {
+        VStack {
+            Text(title).padding()
+
+            TextField("Insert Title", text: $titleInput) // 双向绑定，加上$前缀
+                .textFieldStyle(.roundedBorder)
+        }
     }
 }
 
